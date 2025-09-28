044554a540171f2a52bf6d76bc19de1da50352ec
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-09-28 18:54:41 -0400

    Created consts for colour maps

--- src/components/luicomponents/button.tsx
+++ src/components/luicomponents/button.tsx
@@ -1,34 +1,9 @@
 "use client";
 import React from "react";
 import { Urbanist } from 'next/font/google';
+import { colourMap, textColourMap, roundMap, borderColourMap } from "./constants/constants";
 
 const urbanist = Urbanist({ subsets: ['latin'], weight: ['400', '700'] });
-
-const colourMap = {
-    red: "bg-red-500",
-    orange: "bg-orange-500",
-    yellow: "bg-yellow-500",
-    green: "bg-green-500",
-    blue: "bg-blue-500",
-    purple: "bg-purple-500",
-    white: "bg-white",
-    black: "bg-black",
-    gray: "bg-gray-500",
-    default: "bg-gray-950",
-};
-
-const textColourMap = {
-    red: "text-red-500",
-    orange: "text-orange-500",
-    yellow: "text-yellow-500",
-    green: "text-green-500",
-    blue: "text-blue-500",
-    purple: "text-purple-500",
-    white: "text-white",
-    black: "text-black",
-    gray: "text-gray-500",
-    default: "text-white",
-};
 
 const sizeMap = {
     small: "text-md",
@@ -36,30 +11,26 @@ const sizeMap = {
     large: "text-xl",
 };
 
-const roundMap = {
-    0: "",
-    1: "rounded-md",
-    2: "rounded-xl",
-    3: "rounded-full",
-};
-
 type ButtonProps = {
     label: string;
     onClick: () => void;
     colour?: keyof typeof colourMap;
     textColour?: keyof typeof textColourMap;
+    borderColour?: keyof typeof borderColourMap;
     size?: keyof typeof sizeMap;
     hoverShadow?: boolean;
     round?: keyof typeof roundMap;
 };
 
-export default function Button({ label, onClick, colour = "default", textColour = "default", size = "medium", hoverShadow = false, round = 1 }: ButtonProps) {
+export default function Button({ label, onClick, colour = "white", textColour = "default", size = "medium", borderColour = "black", hoverShadow = false, round = 1 }: ButtonProps) {
     return (
         <button
             onClick={onClick}
             className={`
+                border-2
                 ${colourMap[colour]} 
                 ${textColourMap[textColour]} 
+                ${borderColourMap[borderColour]} 
                 ${urbanist.className} 
                 ${hoverShadow ? "hover:shadow-xl" : ""}
                 ${roundMap[round]}
---
+++ src/components/luicomponents/constants/constants.ts
@@ -0,0 +1,58 @@
+export const colourMap = {
+    red: "bg-red-500",
+    orange: "bg-orange-500",
+    yellow: "bg-yellow-500",
+    green: "bg-green-500",
+    blue: "bg-blue-500",
+    purple: "bg-purple-500",
+    white: "bg-white",
+    black: "bg-black",
+    gray: "bg-gray-500",
+    default: "bg-gray-950",
+};
+
+export const textColourMap = {
+    red: "text-red-500",
+    orange: "text-orange-500",
+    yellow: "text-yellow-500",
+    green: "text-green-500",
+    blue: "text-blue-500",
+    purple: "text-purple-500",
+    white: "text-white",
+    black: "text-black",
+    gray: "text-gray-500",
+    default: "text-black",
+};
+
+export const placeholderColourMap = {
+    red: "placeholder-red-500",
+    orange: "placeholder-orange-500",
+    yellow: "placeholder-yellow-500",
+    green: "placeholder-green-500",
+    blue: "placeholder-blue-500",
+    purple: "placeholder-purple-500",
+    white: "placeholder-white",
+    black: "placeholder-black",
+    gray: "placeholder-gray-500",
+    default: "placeholder-gray-500",
+};
+
+export const borderColourMap = {
+    red: "border-red-500",
+    orange: "border-orange-500",
+    yellow: "border-yellow-500",
+    green: "border-green-500",
+    blue: "border-blue-500",
+    purple: "border-purple-500",
+    white: "border-white",
+    black: "border-black",
+    gray: "border-gray-500",
+    default: "border-black",
+}
+
+export const roundMap = {
+    0: "",
+    1: "rounded-md",
+    2: "rounded-xl",
+    3: "rounded-full",
+};
