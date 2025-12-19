e9b77f09f70814448f3c55999cd954b26f06ed4b
Cristian Diaconu <cristian.diaconu0423@gmail.com>
2025-12-18 19:59:10 -0500

    Edited slug layout

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -13,29 +13,50 @@ export default function RootLayout({
 }>) {
   return (
     <div className="bg-white flex flex-col items-center justify-center py-28">
-        <Navbar/>
+      <Navbar/>
 
-        <div className="min-h-screen w-full max-w-screen-xl mx-auto px-6 flex flex-row bg-white">
-            <div className="grid grid-cols-4 gap-4">
-                <div className="flex flex-col gap-2 justify-start items-start">
-                    <p className={`${urbanist.className} text-md text-gray-800 font-semibold`}> Components </p>
-                    
-                    {components.map((c) => (
-                      <Link
-                        key={c.slug}
-                        href={`/docs/components/${c.slug}`}
-                        className={`${urbanist.className} text-md text-gray-800`}
-                      > 
-                        {c.name}
-                      </Link>
-                    ))}
-                </div>
+      <div className="min-h-screen w-full max-w-screen-xl mx-auto px-6 flex flex-row bg-white">
+        <div className="grid grid-cols-4 gap-4">
+              
+          <div className="flex flex-col gap-4 justify-start items-start">
+            <div className="flex flex-col gap-2">
+              <p className={`${urbanist.className} text-md text-gray-800 font-semibold`}> Getting Started </p>
 
-                <div className="col-span-3">
-                    {children}
-                </div>
+              <Link
+                  href={`/docs/components/`}
+                  className={`${urbanist.className} text-md text-gray-800`}
+                > 
+                  Installation
+              </Link>
+              <Link
+                  href={`/docs/components/`}
+                  className={`${urbanist.className} text-md text-gray-800`}
+                > 
+                  What is LandUI?
+              </Link>
             </div>
+            
+            <div className="flex flex-col gap-2">
+              <p className={`${urbanist.className} text-md text-gray-800 font-semibold`}> Components </p>
+                    
+              {components.map((c) => (
+                <Link
+                  key={c.slug}
+                  href={`/docs/components/${c.slug}`}
+                  className={`${urbanist.className} text-md text-gray-800`}
+                > 
+                  {c.name}
+                </Link>
+              ))}
+            </div>
+            
+          </div>
+
+          <div className="col-span-3">
+            {children}
+          </div>
         </div>
+      </div>
     </div>
   );
 }
